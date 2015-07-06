b978960ef340dde7e64fd7e11f2ded7487f14f0a
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2015-07-06 16:06:11 +0200

    Update 2015-07-06T14:06:10.840Z

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -13083,7 +13083,7 @@ return function (global, window, document, undefined) {
 Velocity, however, doesn't make this distinction. Thus, converting to or from the % unit with these subproperties
 will produce an inaccurate conversion value. The same issue exists with the cx/cy attributes of SVG circles and ellipses. */
 },{}],4:[function(require,module,exports){
-shouldLog = false;
+var shouldLog = false;
 
 module.exports = function(msg){
     if(shouldLog === true) {
@@ -13091,57 +13091,85 @@ module.exports = function(msg){
     }
 };
 },{}],5:[function(require,module,exports){
-var $ = require('jquery'),
-    log = require('./logger.js'),
+var $ = window.jQuery = window.$ = require('jquery');
+require('velocity-animate');
+
+var log = require('./logger.js'),
+    propertyArr = [],
     posts = $('.post'),
-    vel = require('velocity-animate');
+    animRunning = false;
 
-var heightArr = [];
-
-var hideArticles = function(exception){
-    var chosenPosts = $('.post:not(.' + exception + ')');
+var setup = function(){
 
     //Specify height for animation to work
-    for (var i = 0; i < chosenPosts.length; i++) {
-        heightArr[i] = $(chosenPosts[i]).height();
-        $(chosenPosts[i]).height(heightArr[i]);
+    for (var i = 0; i < posts.length; i++) {
+        propertyArr[i] = {
+            h: $(posts[i]).outerHeight(),
+            mt: $(posts[i]).css('margin-top'),
+            mb: $(posts[i]).css('margin-bottom'),
+            pt: $(posts[i]).css('padding-top'),
+            pb: $(posts[i]).css('padding-bottom'),
+        };
+
+        $(posts[i]).attr('data-filter', i);
     }
 
-    chosenPosts.addClass('hidden');
 };
 
-var showArticles = function(){
-    posts.show();
-    posts.removeClass('hidden');
+var hideArticles = function(exception){
+
+    for (var i = 0; i < posts.length; i++) {
+        var $post = $(posts[i]);
+
+        if( $post.hasClass(exception) || exception === false ) {
+
+            var postProps = propertyArr[$post.data('filter')];
+
+            $post.velocity({ 
+                p: {
+                    height: postProps.h,
+                    marginTop: postProps.mt,
+                    marginBottom: postProps.mb,
+                    paddingTop: postProps.pt,
+                    paddingBottom: postProps.pb
+                },
+                o: { duration: 300 }
+            });
+
+        } else {
+
+            $post.velocity({ 
+                p: { height: 0, marginTop: 0, marginBottom: 0, paddingTop: 0, paddingBottom: 0 },
+                o: { duration: 300 }
+            });
+
+        }
+    }
 };
 
 
 $('.filter').find('li').on('click', function(e){
 
-    showArticles();
+    // If animation isn't running, run the functions
+    if ( !animRunning ) {
 
-    if ( $(this).hasClass("filter-button-cases") ){
+        if ( $(this).hasClass('filter-button-cases') ){
 
-        hideArticles("case");
+            hideArticles('case');
 
-    } else if ( $(this).hasClass("filter-button-articles") ) {
-        
+        } else if ( $(this).hasClass('filter-button-articles') ) {
+            
+            hideArticles('article');
 
-        hideArticles("article");
+        } else {
 
+            hideArticles(false);    
+
+        }    
     }
 
 });
 
-// Set non-hidden articles to height auto on browser resize
-$( window ).resize(function() {
-    for (var i = 0; i < posts.length; i++) {
-        if( !$(posts[i]).hasClass('hidden') ) {
-            posts.height('auto');
-        }
-    }
-});
-
+setup();
 log('Filter btns loaded');
-
 },{"./logger.js":4,"jquery":2,"velocity-animate":3}]},{},[1]);
